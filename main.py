aabf0d0939bd76e45901541e8d0407e3c731b872
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-01 20:23:16 +0330

    add lifespan and pooling in start

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.sources.telegram import pooling 
+import asyncio
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        task = asyncio.create_task(pooling())
+        yield
+
+    finally:
+        task.cancel()
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def root():
+    return {
+        "message": {
+            "text":"wellcome to chatbot",
+            "bot": "https://t.me/my_chatbot_ai_botss"
+            }
+        }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000)
