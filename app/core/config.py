c60dba0358837703bdd878855e1ccef97f9734ce
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-01 20:23:51 +0330

    make simpler

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings(BaseSettings):
-    bot_token: str = Field(..., env="BOT_TOEKN")    
+    bot_token: str
     
     class Config:
         env_file = ".env"
