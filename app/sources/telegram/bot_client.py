0addd573e9bece3b76be6c8423aa42b8b186fdb8
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-01 20:25:37 +0330

    - syntax error sync httpx to async httpx - rasise_for_status -  timeout for getUpdate

--- app/sources/telegram/bot_client.py
+++ app/sources/telegram/bot_client.py
@@ -4,26 +4,34 @@ from app.core import settings
 class BotClient:
     def __init__(self):
         self.base_url = f"https://api.telegram.org/bot{settings.bot_token}"
-        self.client = httpx.Client(base_url=self.base_url)
+        self.client = httpx.AsyncClient(
+            base_url=self.base_url,
+            timeout=httpx.Timeout(15.0),
+        )
 
     async def close(self):
         await self.client.aclose()
 
     async def sendMessage(self, chat_id: str, text: str) -> dict:
-        return await self.client.post(
+        r = await self.client.post(
             "/sendMessage",
-            params=
-            {
+            params={
                 "chat_id": chat_id,
                 "text": text,
-            }
+            },
         )
+        r.raise_for_status()
+        return r.json()
 
-    async def getUpdates(self, offset=0, limit=100) -> tuple:
-        return await self.client.get(
+    async def getUpdates(self, offset: int = 0, timeout: int = 10) -> dict:
+        print("get updates")
+
+        r = await self.client.get(
             "/getUpdates",
-            params=
-            {
+            params={
                 "offset": offset,
-            }
-        ), offset
+                "timeout": timeout,
+            },
+        )
+        r.raise_for_status()
+        return r.json()
