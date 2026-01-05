0600341e055acc3172d2909e2f71d36ae828dd28
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-06 02:35:10 +0330

    fix: name

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,9 +1,10 @@
 from .deps import get_db
 from .session import engine
+from .base import Base
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(conn.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 __all__ = [
     "get_db",
