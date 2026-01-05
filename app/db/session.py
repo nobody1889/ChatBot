0eeb82346cbc0e96fbf487688cb3ab05861d4868
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-05 23:46:29 +0330

    creating engine - add pooling connection - async
    createing sessionLocal
    - async

---
+++ app/db/session.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from app.core import settings
+
+engine = create_async_engine(
+    url=settings.database_url,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=3600,
+    echo=False,
+    pool_pre_ping=True,
+)
+
+async_sessionLocal = sessionmaker(
+    engine, 
+    class_=AsyncSession,
+    expire_on_commit=False
+)
