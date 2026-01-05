6ec993f71a56669538c6b6d91f5af712a4cde972
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-06 02:44:39 +0330

    fix : take await from task.close()
    add: more except for end of project
    
    add: logger for main

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.services.telegram import polling
 from app.db import init_db
+from app.core import logging
 import asyncio
+
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    task = None
     try:
         await init_db() # init database
+        logger.info("Database initialized successfully")
+
         task = asyncio.create_task(polling())   # strat the telegram pooling
+
         yield
 
     finally:
-        await task.cancel()
+        try:
+            task.cancel()
+            await task
+        except asyncio.CancelledError:
+            pass
+        except UnboundLocalError:
+            logger.error("task not found")
+        except Exception as e:
+            logger.error(f"Error during shutdown: {e}", exc_info=True)
+            
 
 app = FastAPI(lifespan=lifespan)
 
