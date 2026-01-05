2b471b82c104241e44261013828cdc0b1d8e26bd
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-05 23:42:08 +0330

    add database_url  in config

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,15 @@
 from pydantic_settings import BaseSettings
-from pydantic import Field
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings(BaseSettings):
     bot_token: str
+
     ollama_url: str
     ollama_model: str
+
+    database_url: str
     
     class Config:
         env_file = ".env"
