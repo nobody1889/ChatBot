82dca5c2865ec2903a63fb9b0da104284d8a4af9
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-07 02:11:16 +0330

    remove: commit from user repo

--- app/services/accounts/user_repository.py
+++ app/services/accounts/user_repository.py
@@ -5,53 +5,34 @@ from typing import Optional
 from sqlalchemy import select, update
 
 class UserRepository:
-    def __init__(self, db: AsyncSession) -> None:
+    def __init__(self, db: AsyncSession):
         self.db = db
 
     async def get_by_user_id(self, user_id: str) -> Optional[User]:
-        result = await self.db.execute(
-            select(User).where(User.user_id == user_id)
-        )
+        result = await self.db.execute(select(User).where(User.user_id == user_id))
         return result.scalar_one_or_none()
 
     async def get_by_username(self, username: str) -> Optional[User]:
-        result = await self.db.execute(
-            select(User).where(User.username == username)
-        )
+        result = await self.db.execute(select(User).where(User.username == username))
         return result.scalar_one_or_none()
 
     async def create(self, user: UserCreate) -> User:
         user_db = User(**user.model_dump())
+        self.db.add(user_db)
         
-        await self.db.add(user_db)
-        await self.db.commit()
-        await self.db.refresh(user_db)
-        
+        await self.db.flush()
         return user_db
 
-    async def update(self, user: UserUpdate, data: UserUpdate) -> User:
+    async def update(self, user: User, data: UserUpdate) -> User:
         update_data = data.model_dump(exclude_unset=True)
         for key, value in update_data.items():
             setattr(user, key, value)
-
-        await self.db.commit()
-        await self.db.refresh(user)
-
+        await self.db.flush()
         return user
-        
-
-    async def update_or_create(self, user: UserCreate) -> User:
-        exis = await self.get_by_user_id(user.user_id)
-        
-        if exis:
-            return await self.update(exis, user)
-
-        return await self.create(user)
-    
 
     async def set_block(self, user_id: str, block: bool) -> bool:
         result = await self.db.execute(
             update(User).where(User.user_id == user_id).values(is_blocked=block)
         )
-        await self.db.commit()
-        return result.rowcount > 0 
+        await self.db.flush()
+        return result.rowcount > 0
