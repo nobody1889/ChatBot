3e68b96f1511c669280ec186af343f9877f09bb7
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-07 02:09:37 +0330

    add: commit

--- app/db/deps.py
+++ app/db/deps.py
@@ -2,4 +2,10 @@ from .session import async_sessionLocal
 
 async def get_db():
     async with async_sessionLocal() as session:
-        yield session
+        try:
+            yield session
+            await session.commit()
+            
+        except Exception as e:
+            await session.rollback()
+            raise e
