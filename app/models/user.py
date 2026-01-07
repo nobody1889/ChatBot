d0d740c82a9a73398d2607e2446ace43183aca54
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-07 19:36:31 +0330

    add: Intger in id

--- app/models/user.py
+++ app/models/user.py
@@ -1,20 +1,20 @@
 from ..db import Base
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import relationship, mapped_column, Mapped
+from sqlalchemy import String, Boolean, Integer
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.sql import func
 from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-
-    username: Mapped[str] = mapped_column(String(255), unique=True, index=True)
-
-    firsname: Mapped[str] = mapped_column(String(255), nullable=True)
-    lastname: Mapped[str] = mapped_column(String(255), nullable=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
 
     user_id: Mapped[str] = mapped_column(String(255), unique=True, index=True)
 
+    username: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=True)
+    first_name: Mapped[str] = mapped_column(String(255), nullable=True)  # fixed typo
+    last_name: Mapped[str] = mapped_column(String(255), nullable=True)   # fixed typo
+
     is_blocked: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+    created_at: Mapped[datetime] = mapped_column(default=func.now())  # safer than datetime.now()
