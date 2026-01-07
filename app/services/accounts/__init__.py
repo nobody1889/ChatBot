7a44926a2bd1d0fd10a3b9367e71418ecbd7eda7
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-07 03:46:22 +0330

    add: UserRepository

--- app/services/accounts/__init__.py
+++ app/services/accounts/__init__.py
@@ -1,3 +1,3 @@
-from .deps import get_user_service, UserService
+from .deps import get_user_service, UserService, UserRepository
 
-__all__ = ["get_user_service", "UserService"]
+__all__ = ["get_user_service", "UserService", "UserRepository"]
