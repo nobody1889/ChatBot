40297f87266833ccade9c3f64387f3077b1f45a0
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-09 00:13:10 +0330

    add: new router for ai

--- app/api/route.py
+++ app/api/route.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .user import router as user_router
+from .ai import router as ai_router
 
 router = APIRouter()
 
 router.include_router(user_router)
+router.include_router(ai_router)
