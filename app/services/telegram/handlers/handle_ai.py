fe1fb109b750a379ff6a6fc4a424e819ff723932
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-09 00:14:48 +0330

    moved: from assistant to telegram

---
+++ app/services/telegram/handlers/handle_ai.py
@@ -0,0 +1,17 @@
+from app.services.assistant import AiClient
+from app.core.logging import logging
+
+logger = logging.getLogger(__name__)
+
+async def handle_ai_message(bot, chat_id, message):
+    ai = AiClient()
+    try:
+        logger.info(f"ai client created for chat_from: {chat_id} and message: {message}",)
+        response = await ai.chat(message=message)
+        response = await bot.sendMessage(chat_id, response)
+    except Exception as e:
+        logger.error(e)
+    finally:
+        await ai.close()
+        logger.info("ai client closed")
+
