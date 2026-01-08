c6b7f0659f65cde43add1cf2cd1701030921ee20
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-09 00:15:14 +0330

    create: api for ai message

---
+++ app/api/ai.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.schemas import AiTextResponse, AiTextRequest
+from app.core import logging
+from app.services.assistant import AiClient
+
+logger = logging.getLogger(__name__)
+
+router = APIRouter(
+    prefix="/api/ai",
+    tags=["Ai"],
+)
+
+@router.post('/message', response_model=AiTextResponse)
+async def message_request(request_body: AiTextRequest):
+    try:
+        message: str = request_body.get("message")
+
+        if not message:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Missing 'message' field in request body."
+            )
+        
+        ai = AiClient()
+        respopnse:AiTextResponse = await ai.chat(message=message)
+
+        return {
+            "status": "success",
+            "message": respopnse
+        }
+    
+    except Exception as e:
+        logger.error(f"Error while Ai message request: {e}")
+
