a2c3741e4a35c197eae70cffe13c21979f6de01b
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-09 16:27:28 +0330

    add: Assistant schemas

--- app/schemas/ai.py
+++ app/schemas/ai.py
@@ -2,6 +2,25 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
+class AssistantBase(BaseModel):
+    user_id: int
+    name: str
+    model: Optional[str] = None
+
+class AssistantCreate(AssistantBase):
+    pass
+
+class AssistantRead(AssistantBase):
+    id: int
+    created_at: datetime
+
+class AssistantDelete(BaseModel):
+    user_id: int
+    name: int
+
+class AssistantUpdate(AssistantBase):
+    pass
+
 class AiTextRequest(BaseModel):
     message: str
 
