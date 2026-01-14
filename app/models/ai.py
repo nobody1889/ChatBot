81673ad14539444c67adbed6df3f67bf9d690cfc
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-14 04:43:54 +0330

    add: back_populates and user feild

--- app/models/ai.py
+++ app/models/ai.py
@@ -1,5 +1,5 @@
 from ..db import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.sql import func
 from datetime import datetime
@@ -13,5 +13,6 @@ class Assistant(Base):
     model: Mapped[str] = mapped_column(String(255))
 
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
-    
+    user = relationship("User", back_populates="assistants")
+
     created_at: Mapped[datetime] = mapped_column(default=func.now()) 
