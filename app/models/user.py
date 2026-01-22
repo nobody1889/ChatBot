96819290b2eaba9863df05efdea1d9dd8667f114
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-22 06:57:30 +0330

    fix: for loading

--- app/models/user.py
+++ app/models/user.py
@@ -16,6 +16,6 @@ class User(Base):
     last_name: Mapped[str] = mapped_column(String(255), nullable=True)
 
     is_blocked: Mapped[bool] = mapped_column(Boolean, default=False)
-    assistants: Mapped[list[Assistant]] = relationship("Assistant", back_populates="user")
+    assistants: Mapped[list[Assistant]] = relationship("Assistant", back_populates="user", lazy="selectin")
 
     created_at: Mapped[datetime] = mapped_column(default=func.now())
