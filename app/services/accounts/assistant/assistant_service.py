fd75b16ca1d6b6cb2fae1d38c3f17810b71aa5e5
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-22 10:11:25 +0330

    add: create_assistant get_user delete to service

--- app/services/accounts/assistant/assistant_service.py
+++ app/services/accounts/assistant/assistant_service.py
@@ -1,2 +1,18 @@
+from .assistant_repository import AssistantRepository
+from app.schemas.ai import AssistantCreate, AssistantDelete
+
 class AssistantService:
-    pass
+    def __init__(self, assistant_repo: AssistantRepository):
+        self.assistant_repo = assistant_repo
+
+    async def create_assistant(self, assistant: AssistantCreate):
+        return self.assistant_repo.create(assistant)
+    
+    async def get_user_assistants(self, user_id: int):
+        return await self.assistant_repo.get_by_user_id(user_id)
+
+    async def get_user_assistant(self, user_id: int, name: str):
+        return await self.assistant_repo.get_by_name(user_id, name)
+    
+    async def delete(self, assistant: AssistantDelete):
+        return await self.assistant_repo.delete(assistant)
