f4e57303bf4dc7c78c45095b9dd96fb152141d50
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-23 09:31:10 +0330

    add: some try except

--- app/api/v1/assistant.py
+++ app/api/v1/assistant.py
@@ -13,24 +13,43 @@ router = APIRouter(
 
 @router.post("/", response_model=AssistantRead)
 async def create_assistant(data: AssistantCreate, service: AssistantService = Depends(get_assistant_service)):
-    logger.info(f"Creating assistant {data.name} for user {data.user_id}")
-    return await service.create_assistant(data)
+    try:
+        assistant_obj: AssistantRead = await service.create_assistant(data)
+        logger.info(f"Assistant created: {assistant_obj.name} for user {assistant_obj.user_id}")
+        return assistant_obj
+    except Exception as e:
+        logger.error(f"Error creating assistant: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 @router.get("/user/{user_id}", response_model=list[AssistantRead])
 async def get_user_assistants(user_id: str, service: AssistantService = Depends(get_assistant_service)):
-    return await service.get_user_assistants(user_id)
+    try:
+        assistants: list[AssistantRead] = await service.get_user_assistants(user_id)
+        logger.info(f"Retrieved {len(assistants)} assistants for user {user_id}")
+        return assistants
+    except Exception as e:
+        logger.error(f"Error retrieving assistants for user {user_id}: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @router.get("/user/{user_id}/name/{name}", response_model=AssistantRead)
 async def get_user_assistant(user_id: str, name: str, service: AssistantService = Depends(get_assistant_service)):
-    assistant = await service.get_user_assistant(user_id, name)
-    if not assistant:
-        raise HTTPException(status_code=404, detail="Assistant not found")
-    return assistant
+    try:
+        assistant = await service.get_user_assistant(user_id, name)
+        if not assistant:
+            raise HTTPException(status_code=404, detail="Assistant not found")
+        return assistant
+    except Exception as e:
+        logger.error(f"Error retrieving assistant {name} for user {user_id}: {e}")
+        raise 
 
 
 @router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_assistant( data: AssistantDelete, service: AssistantService = Depends(get_assistant_service)):
-    success = await service.delete(data)
-    if not success:
-        raise HTTPException(status_code=404, detail="Assistant not found")
+    try:
+        success = await service.delete(data)
+        if not success:
+            raise HTTPException(status_code=404, detail="Assistant not found")
+    except Exception as e:
+        logger.error(f"Error deleting assistant: {e}")
+        raise 
