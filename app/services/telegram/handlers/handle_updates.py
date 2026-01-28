0a0a3f0323d84524f721f622dcd988b3b814e970
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-28 12:19:18 +0330

    fix: get_or_create_user get UserCreate so we create it first and pass it next fix: both handlers do not need user_service

--- app/services/telegram/handlers/handle_updates.py
+++ app/services/telegram/handlers/handle_updates.py
@@ -1,7 +1,7 @@
 from .handle_commands import command_handler
 from .handle_ai import handle_ai_message
 from app.services.accounts import UserService
-
+from app.schemas import UserCreate
 
 async def dispatcher(bot, update, user_service: UserService):
     if "message" not in update:
@@ -11,21 +11,22 @@ async def dispatcher(bot, update, user_service: UserService):
     chat_id = str(message["chat"]["id"])
     text: str = message.get("text")
 
-    user = await user_service.get_or_create_user(
+    user_create = UserCreate(
         user_id=chat_id,
         username=message["from"].get("username"),
         first_name=message["from"].get("first_name"),
         last_name=message["from"].get("last_name"),
-    )
-
+        ) 
+    
+    user = await user_service.get_or_create_user(user_create)
     if user.is_blocked:
         await bot.sendMessage(chat_id, "🚫 You are blocked.")
         return
 
     if text:
         if text.startswith("/"):
-            await command_handler(bot, chat_id, text, user_service)
+            await command_handler(bot, chat_id, text)
         else:
-            await handle_ai_message(bot, chat_id, text, user_service)
+            await handle_ai_message(bot, chat_id, text)
     else:
         await bot.sendMessage(chat_id, "i just handle text message 💔💔")
