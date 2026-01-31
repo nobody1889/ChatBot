ceacbca8b0961011828c7c270c50f54feea1e547
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-31 16:30:31 +0330

    fix: all get in repo come from an order to set home 0 as default in all times add: set_assistant_default for user switch between ai models

--- app/services/accounts/assistant/assistant_repository.py
+++ app/services/accounts/assistant/assistant_repository.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas.assistant import AssistantCreate, AssistantDelete, AssistantRead
 from app.models.assistant import Assistant
-from sqlalchemy import select
+from sqlalchemy import select, update
 from typing import List, Optional
+from sqlalchemy.sql import func
 
 class AssistantRepository:
     def __init__(self, db: AsyncSession):
@@ -14,19 +15,31 @@ class AssistantRepository:
         await self.db.flush()
         return assisstant
 
-    async def get_by_user_id(self, user_id: int)-> List:
-        query = select(Assistant).where(Assistant.user_id == user_id)
+    async def get_by_user_id(self, user_id: int) -> List:
+        query = select(Assistant).where(Assistant.user_id == user_id).order_by(Assistant.last_used_at.desc())
         result = await self.db.execute(query)
         return result.scalars().all()
         
 
     async def get_by_model(self, user_id: int, model: str) -> Optional[Assistant]:
-        query = select(Assistant).where(Assistant.user_id == user_id)
+        query = select(Assistant).where(Assistant.user_id == user_id).order_by(Assistant.last_used_at.desc())
         query = query.where(Assistant.model == model)
         result = await self.db.execute(query)
         return result.scalar_one_or_none()
 
-    
+    async def set_assistant_default(self, user_id: int, model: str) -> List:
+        query = (
+        update(Assistant)
+        .where(Assistant.user_id == user_id)
+        .where(Assistant.model == model)
+        .values(last_used_at=func.now())
+        )
+
+        await self.db.execute(query)
+        await self.db.flush()
+
+        return await self.get_by_user_id(user_id)
+
     async def delete(self, data: AssistantDelete)-> bool:
         query = select(Assistant).where(Assistant.user_id == data.user_id)
         
