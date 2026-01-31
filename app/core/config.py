e2411d0b7deafc4bcb8026e6cc101b2ee7875aa7
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-31 12:18:49 +0330

    fix: we have list models improve: useing model_config to go from load_dotenv

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,19 @@
-from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Json
+from typing import List
 
 class Settings(BaseSettings):
     bot_token: str
     telegram_bot_link: str
 
     ollama_url: str
-    ollama_model: str
+    ollama_models: Json[List[str]]
 
     database_url: str
     
-    class Config:
-        env_file = ".env"
-
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
 
 settings = Settings()
