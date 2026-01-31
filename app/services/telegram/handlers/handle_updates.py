a06b0c4f3c4facc5fb7b8175ceda92f1ee36bf20
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-31 07:21:15 +0330

    fix: change if else to match case clean: add two handle to make dispatcher cleaner add: callback query

--- app/services/telegram/handlers/handle_updates.py
+++ app/services/telegram/handlers/handle_updates.py
@@ -1,23 +1,27 @@
 from .handle_commands import command_handler
 from .handle_ai import handle_ai_message
 from .user_handler import UserHandler
+from .handle_callback_query import handle_callback_query
 
-async def dispatcher(bot, update: dict):
-    if "message" not in update:
-        return
-    
+async def handle_user(bot, data: dict) -> dict | None:
+    chat_id = str(data["chat"]["id"])
+
     handler = UserHandler(bot=bot)
-
-    message: dict = update["message"]
-    chat_id = str(message["chat"]["id"])
-    text: str = message.get("text")
-
-    
-    user: dict = await handler.get_or_create_user(data = message)
+    user: dict = await handler.get_or_create_user(data = data)
 
     if user.get("is_blocked"):
         await bot.sendMessage(chat_id, "🚫 You are blocked.")
         return
+    
+    return user
+    
+async def handle_message(bot, message: dict) -> None:
+    user = await handle_user(bot, message)
+    if not user:
+        return
+
+    text: str | None = message.get("text")
+    chat_id: str = str(message["chat"]["id"])
 
     if text:
         if text.startswith("/"):
@@ -26,3 +30,18 @@ async def dispatcher(bot, update: dict):
             await handle_ai_message(bot, chat_id, text)
     else:
         await bot.sendMessage(chat_id, "i just handle text message 💔💔")
+    
+async def dispatcher(bot, update: dict):
+    match update:
+        case {"callback_query": callback_query}:
+            await handle_callback_query(bot, callback_query)
+        
+        case {"message": message}:
+            await handle_message(bot, message)
+
+        case _:
+            return
+    
+    
+
+    
