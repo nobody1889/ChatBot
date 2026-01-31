591e96e7e014684402dc1292c4491919e6819793
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-01-31 11:31:20 +0330

    fix: this handler get user to check if user has assitant or not add: simple help if assistant is not there add: reply for better answer

--- app/services/telegram/handlers/handle_ai.py
+++ app/services/telegram/handlers/handle_ai.py
@@ -1,14 +1,33 @@
 from app.services.assistant import AssistantClient as AiClient
 from app.core.logging import logging
+from app.services.telegram.bot_client import BotClient
 
 logger = logging.getLogger(__name__)
 
-async def handle_ai_message(bot, chat_id, message):
+async def handle_ai_message(bot: BotClient, user: dict, message: str, message_id: int) -> None:
     ai = AiClient()
+
+    chat_id = user.get("user_id")
+    assistants: list = user.get("asstants")
+
+    if not assistants:
+        await bot.sendMessage(
+            chat_id=chat_id,
+            text="😔 You don't have any assistants yet\n\n"
+            "Use /new_chat to create one\n"
+            "Use /mylist to see your assistants",
+            reply_message_id=message_id,
+        )
+        return
+
     try:
         logger.info(f"ai client created for chat_from: {chat_id} and message: {message}",)
         response = await ai.chat(message=message)
-        response = await bot.sendMessage(chat_id, response)
+        response = await bot.sendMessage(
+            chat_id, 
+            response
+            
+            )
     except Exception as e:
         logger.error(e)
     finally:
