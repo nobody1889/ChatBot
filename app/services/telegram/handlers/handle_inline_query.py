28e6fa2d5173f1319af561f333b449845262b0ed
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-02-01 14:04:13 +0330

    add: handle inline query

---
+++ app/services/telegram/handlers/handle_inline_query.py
@@ -0,0 +1,7 @@
+from ..bot_client import BotClient
+
+async def handle_inline_query(bot: BotClient, inline_query: dict) -> None:
+    pass
+
+async def handle_chosen_inline_result(bot: BotClient, chosen_inline_result: dict) -> None:
+    pass
