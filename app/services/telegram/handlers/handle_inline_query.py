8fc35c4087faf69eebec0f02a5dd36c7300cdf50
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-02-06 22:38:13 +0330

    handle: inline query load assistant

--- app/services/telegram/handlers/handle_inline_query.py
+++ app/services/telegram/handlers/handle_inline_query.py
@@ -1,7 +1,41 @@
 from ..bot_client import BotClient
+from app.core import settings
+
+async def load_assistants(offset: int) -> list[dict]:
+    models:list = settings.ollama_models
+    result = []
+
+    start = (offset-1) * 10
+    end = offset * 10
+
+    for model in models[start:end]:
+        result.append({
+            "type": "article",
+            "id": model,
+            "title": model,
+            "description": model,
+            "input_message_content": {
+                "message_text": model,
+            },
+        })
+
+    return result
+
+async def load_users(offset: int) -> list[dict]:
+    pass
 
 async def handle_inline_query(bot: BotClient, inline_query: dict) -> None:
-    pass
+    query: str = inline_query["query"]
+    offset = int(inline_query.get("offset", 1))
+
+    if "assistant" in query.split["_"]:
+        result = await load_assistants(offset)
+    if "user" in query.split["_"]:
+        result = await load_users(offset)
+
+    await bot.answer_inline_query(inline_query["id"], result)
+
+    
 
 async def handle_chosen_inline_result(bot: BotClient, chosen_inline_result: dict) -> None:
-    pass
+    print(f"Received chosen inline result: {chosen_inline_result}")
