3446b3eca7a7abe6f3daf6eed34f53ddf55ec7a2
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-02-11 09:56:56 +0330

    fix: handle if the query is not exist
    fix: messages from inline query is like commands
    
    fix: names in list to found the target query

--- app/services/telegram/handlers/handle_inline_query.py
+++ app/services/telegram/handlers/handle_inline_query.py
@@ -11,24 +11,23 @@ async def load_assistants(query: str, offset: int) -> list[dict]:
 
     for model in models[start:end]:
         if model.startswith(query):
-            id = str(uuid.uuid4())
             result.append({
                 "type": "article",
-                "id": id,
+                "id": str(uuid.uuid4()),
                 "title": model,
                 "description": model,
                 "input_message_content": {
-                    "message_text": model,
+                    "message_text": "/select_assistant " + model,
                 },
             })
     if not result:
         result.append({
             "type": "article",
             "id": str(uuid.uuid4()),
-            "title": "None",
-            "description": "None",
+            "title": "Not Found",
+            "description": "Not Found",
             "input_message_content": {
-                    "message_text": "None",
+                    "message_text": "Not Found",
                 },
         })
 
@@ -37,17 +36,27 @@ async def load_assistants(query: str, offset: int) -> list[dict]:
 async def load_users(offset: int) -> list[dict]:
     pass
 
+async def load_unknown() -> list[dict]:
+    return [{
+        "type": "article",
+        "id": str(uuid.uuid4()),
+        "title": "Unknown",
+        "description": "Unknown",
+        "input_message_content": {
+                "message_text": "Unknown",
+            },
+    }]
+
 async def handle_inline_query(bot: BotClient, inline_query: dict) -> None:
     query: str = inline_query["query"]
     offset = int(inline_query.get("offset") or 1)
 
-    if "assistant" in query.split("_"):
+    if "assistants" in query.split("_"):
         result = await load_assistants(query=query.split(": ")[-1], offset=offset)
-    elif "user" in query.split("_"):
+    elif "users" in query.split("_"):
         result = await load_users(offset)
     else:
-        await bot.sendMessage(chat_id=inline_query["id"], text="i don't handle this!!!!!")
-        return
+        result = await load_unknown()
     
     await bot.answer_inline_query(inline_query["id"], result)
 
