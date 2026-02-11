d7eef35cb5ea110a8759812fd981431a7792756d
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-02-11 12:12:15 +0330

    update: improve: models may be upper case so we set them all lower
    fix: we need at list a meesge on result
    fix: we passuser_id becuse we need it add loading user assistants

--- app/services/telegram/handlers/features/handle_inline_query.py
+++ app/services/telegram/handlers/features/handle_inline_query.py
@@ -11,7 +11,7 @@ async def load_assistants(query: str, offset: int) -> list[dict]:
     end = offset * 10
 
     for model in models[start:end]:
-        if model.startswith(query):
+        if model.lower().startswith(query.lower()):
             result.append({
                 "type": "article",
                 "id": str(uuid.uuid4()),
@@ -27,13 +27,15 @@ async def load_assistants(query: str, offset: int) -> list[dict]:
             "id": str(uuid.uuid4()),
             "title": "Not Found",
             "description": "Not Found",
-            "switch_inline_query_current_chat": ""
+            "input_message_content": {
+                    "message_text": "NO MODEL",
+                },
         })
 
     return result
 
-async def load_my_assistants(offset: int) -> list[dict]:
-    assistants = await AssistantHandler().get_user_assistants(user_id=offset)
+async def load_my_assistants(user_id: str, offset: int) -> list[dict]:
+    assistants = await AssistantHandler().get_user_assistants(user_id=user_id)
     result = []
 
     for assistant in assistants:
@@ -52,7 +54,9 @@ async def load_my_assistants(offset: int) -> list[dict]:
             "id": str(uuid.uuid4()),
             "title": "No Assistants Found",
             "description": "No Assistants Found",
-            "switch_inline_query_current_chat": ""
+            "input_message_content": {
+                    "message_text": "NO MODEL",
+                },
         })
 
     return result
@@ -75,19 +79,20 @@ async def load_unknown() -> list[dict]:
     }]
 
 LOADERS = {
-    "new_assistant": lambda offset, q: load_assistants(query=q.split(":")[-1].strip(), offset=offset),
-    "assistants_list": lambda offset, q: load_my_assistants(offset=offset),
-    "new_user": lambda offset, q: load_users(offset),
-    "users_list": lambda offset, q: load_my_users(offset),
+    "new_assistant": lambda user_id, offset, q: load_assistants(query=q.split(":")[-1].strip(), offset=offset),
+    "assistants_list": lambda user_id, offset, q: load_my_assistants(user_id=user_id, offset=offset),
+    "new_user": lambda user_id, offset, q: load_users(offset),
+    "users_list": lambda user_id, offset, q: load_my_users(offset),
 }
 
 async def handle_inline_query(bot: BotClient, inline_query: dict) -> None:
     query: str = inline_query["query"]
     offset = int(inline_query.get("offset") or 1)
+    user_id = inline_query["from"]["id"]
 
     for prefix, loader in LOADERS.items():
         if query.startswith(prefix):
-            result = await loader(offset, query)
+            result = await loader(user_id=user_id, offset=offset, q=query)
             break
     else:
         result = await load_unknown()  
