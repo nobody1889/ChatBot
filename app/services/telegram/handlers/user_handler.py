88b7e1eed45a49cb4100edbe8b9372f02f31e93e
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-02-11 09:54:53 +0330

    add: new handler for assistant in bot

--- app/services/telegram/handlers/user_handler.py
+++ app/services/telegram/handlers/user_handler.py
@@ -41,8 +41,49 @@ class UserHandler:
                 return
             
             user = resp.json()
-
-        return user
     
+        return user
+
+    async def close(self):
+        await self._client.aclose()
+
+class AssistantHandler:
+    def __init__(self, bot: BotClient):
+        self.base_url = f"http://localhost:{settings.port}/api/v1/assistant/"
+        self._client = httpx.AsyncClient(
+            base_url=self.base_url,
+            timeout=httpx.Timeout(15.0),
+        )
+
+        self.bot = bot
+    
+    async def set_or_add_assistant(self, user_id: str, assistant_name: str) -> dict | None:
+        resp = await self._client.get( # check if assistant exists
+            f"user/{user_id}/model/{assistant_name}",
+        )
+
+        if resp.status_code == 200: # set default assistant
+            resp = await self._client.put(
+                f"user/{user_id}/model/{assistant_name}",
+            )
+            return resp.json()
+        
+        elif resp.status_code == 404:
+            resp = await self._client.post(
+                json={
+                    "model": assistant_name,
+                    "user_id": user_id
+                    },
+            )
+
+            if resp.status_code == 200:
+                return resp.json()
+        
+        await self.bot.sendMessage(
+            chat_id=user_id,
+            text=f"Failed to select assistant {assistant_name}. Please try again later."
+        )
+        return
+
     async def close(self):
         await self._client.aclose()
